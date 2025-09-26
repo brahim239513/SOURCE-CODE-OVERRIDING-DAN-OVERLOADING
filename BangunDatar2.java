ea5afe0cd53e99efd1fe2e46e60ee0609ae5dbe3
MUHAMMAD IBRAHIEM <MuhammadIbrahiem57@gmail.com>
2025-09-26 07:39:00 +0700

    Update BangunDatar2.java

--- BangunDatar2.java
+++ BangunDatar2.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package overriding2;
+package overloading;
 
 /**
  *
@@ -24,4 +24,5 @@ public class BangunDatar2 {
         System.out.println("LUAS = " + luas);
         System.out.println("KELILING = " + keliling);
     }
+
 }
