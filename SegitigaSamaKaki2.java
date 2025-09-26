73cd8ef110ad6c36dfee7fabde397ff73d9db82c
MUHAMMAD IBRAHIEM <MuhammadIbrahiem57@gmail.com>
2025-09-26 07:38:07 +0700

    Update SegitigaSamaKaki2.java

--- SegitigaSamaKaki2.java
+++ SegitigaSamaKaki2.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package overriding2;
+package overloading;
 
 /**
  *
@@ -40,3 +40,4 @@ public class SegitigaSamaKaki2 extends BangunDatar2 {
         super.tampilData();
     }
 }
+
