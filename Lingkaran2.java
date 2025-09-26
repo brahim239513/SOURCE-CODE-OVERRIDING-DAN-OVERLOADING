a79a8b42753258f5f06de67e81394c0ca71ef603
MUHAMMAD IBRAHIEM <MuhammadIbrahiem57@gmail.com>
2025-09-26 07:38:36 +0700

    Lingkaran2.java

--- Lingkaran2.java
+++ Lingkaran2.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package overriding2;
+package overloading;
 
 /**
  *
@@ -35,4 +35,5 @@ public class Lingkaran2 extends BangunDatar2 {
         System.out.println("JARI-JARI = " + jariJari);
         super.tampilData();
     }
+
 }
